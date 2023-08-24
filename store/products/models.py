6a1f88047bb430eef7a8f6de2b2782b0fa512a9c
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-08-24 14:40:33 +0300

    added model fetching media files configured to folder added __str__ to models

--- store/products/models.py
+++ store/products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name: str = models.CharField(max_length=128, unique=True)
     description: str = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{type(self).__name__}(name={self.name!r})'
+
 
 class Product(models.Model):
     title: str = models.CharField(max_length=256)
@@ -14,3 +17,6 @@ class Product(models.Model):
     img = models.ImageField(upload_to='products_images')
 
     category: int = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{type(self).__name__}(title={self.title!r}, category={self.category})'
--- store/products/views.py
+++ store/products/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
 
 def index(request: HttpRequest):
@@ -12,27 +13,10 @@ def index(request: HttpRequest):
 
 
 def products(request: HttpRequest):
+    Product.objects.all()
     context = {
         'title': 'My products',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-                'title': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-                'price': 2890,
-                'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-            },
-            {
-                'image': '/static/vendor/img/products/Black-Dr-Martens-shoes.png',
-                'title': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-                'price': 13590,
-                'description': 'Гладкий кожаный верх. Натуральный материал.',
-            },
-            {
-                'image': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png',
-                'title': 'Черный рюкзак Nike Heritage',
-                'price': 2340,
-                'description': 'Плотная ткань. Легкий материал.',
-            },
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
     }
     return render(request, 'products/products.html', context=context)
